64d6ecaafcd1e4305bfdee5d86ee46cd32938ac8
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-24 02:26:18 +0530

    minor update/Api gateway

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const setupAndStartServer=()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
-    app.use("/api",apiRoutes);
+    app.use("/bookingservice/api",apiRoutes);
 
 
    app.listen(PORT, ()=>{
